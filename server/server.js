1997df429681eb03b62dc48dace7c5d684b7d264
Appsaloon Toon Nelissen <toon@appsaloon.be>
2019-04-22 17:41:09 +0200

    Remove greenlock

--- server/server.js
+++ server/server.js
@@ -8,11 +8,6 @@ const Luftdaten = require('./luftdaten')
 const irceline = new Irceline()
 const luftdaten = new Luftdaten()
 
-// environment variables
-const noSSL = process.env.NODE_ENV !== 'production'
-const email = process.env.SSL_EMAIL || ''
-const approvedDomains = (process.env.SSL_DOMAINS || '').split(',')
-
 const app = express()
 
 app.use(function (req, res, next) {
@@ -60,41 +55,4 @@ app.get('/luftdaten', (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'build/index.html')))
 
-if (!noSSL) {
-    const greenLock = require('greenlock-express')
-    console.log('using greenlock')
-    greenLock.create({
-
-        // Let's Encrypt v2 is ACME draft 11
-        version: 'draft-11',
-
-        server: 'https://acme-v02.api.letsencrypt.org/directory',
-        // Note: If at first you don't succeed, stop and switch to staging
-        // https://acme-staging-v02.api.letsencrypt.org/directory
-
-        // You MUST change this to a valid email address
-        email,
-
-        // You MUST NOT build clients that accept the ToS without asking the user
-        agreeTos: true,
-
-        // You MUST change these to valid domains
-        // NOTE: all domains will validated and listed on the certificate
-        approvedDomains,
-
-        // You MUST have access to write to directory where certs are saved
-        // ex: /home/foouser/acme/etc
-        configDir: '/etc/greenlock/acme/',
-
-        app,
-
-        // Get notified of important updates and help me make greenlock better
-        communityMember: false
-
-        //, debug: true
-
-    }).listen(80, 443)
-} else {
-    console.log('not using greenlock')
-    app.listen(8080)
-}
+app.listen(80)
